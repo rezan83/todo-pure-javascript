ab4edb001e58f96bf44ccd42dd28b9a3f838abc2
rezan83 <rezan.moh83@gmail.com>
2023-02-04 10:31:44 +0100

    some fixes

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 const state = {
     countId: 1,
     editableTodo: null,
+    showModal:false,
     newContent: "",
     todos: [
         { content: "Orange", done: false, id: -1 },
         { content: "Apple", done: true, id: 0 }
     ],
     addTodo() {
-        this.todos.unshift(this.createTodo(this.newContent));
+        this.todos.unshift({
+            content: this.newContent,
+            done: false,
+            id: this.countId
+        });
         this.countId++;
     },
     removeTodo(id) {
         this.todos = this.todos.filter((todo) => todo.id !== id);
     },
-    toggleTodo(id) {
+    toggleTodoIsDone(id) {
         let todo = this.findTodo(id);
         todo.done = !todo.done;
     },
     findTodo(id) {
         return this.todos.find((todo) => todo.id === id);
-    },
-    createTodo(content) {
-        return { content, done: false, id: this.countId };
     }
+
 };
 
 const app = {
     todosContainerElement: document.querySelector(".todos-container"),
-    todoEditModalElement: document.querySelector(".todo-edit-modal"),
+    todoEditModalContainerElement: document.querySelector(
+        ".todo-edit-modal--container"
+    ),
     todoInputElement: document.getElementById("todoInput"),
     todoFormElement: document.querySelector(".todoForm"),
 
@@ -35,14 +40,9 @@ const app = {
         state.removeTodo(id);
         this.renderTodos(state);
     },
-    toggleTodoHandel(id) {
-        state.toggleTodo(id);
+    toggleTodoIsDoneHandel(id) {
+        state.toggleTodoIsDone(id);
         this.renderTodos(state);
-    },
-    openEditModal(id) {
-        state.editableTodo = state.findTodo(id);
-        this.renderModal(state.editableTodo.content);
-        this.todoEditModalElement.style.display = "grid";
     },
     changeInputHandel(event) {
         state.newContent = event.target.value;
@@ -53,22 +53,32 @@ const app = {
         state.newContent = "";
         this.render(state);
     },
+    openEditModal(id) {
+        state.showModal = true;
+        state.editableTodo = state.findTodo(id);
+        this.renderModal(state.editableTodo.content);
+    },
     closeEditModal() {
-        this.todoEditModalElement.style.display = "none";
-        this.renderTodos(state);
+        state.showModal = false;
+        this.renderModal();
     },
     editTodoHandel(event) {
         state.editableTodo.content = event.target.value;
         this.renderTodos(state);
     },
     renderModal(content = null) {
-        this.todoEditModalElement.innerHTML = `<div class="todo">
+        this.todoEditModalContainerElement.innerHTML = `<div class="todo-edit-modal ${
+            state.showModal && "todo-edit-modal--show"
+        }">
+    
+        <div class="todo">
                 <label for="todo-edit">Edit Todo:</label>
                 <input id="todo-edit" class="input-content" name="todo-edit" onchange="app.editTodoHandel(event)" value="${content}"/>
                 <div>
                     <button class="btn btn-close" onclick="app.closeEditModal()">done</button>
                 </div>
-            </div>`;
+            </div>
+        </div>`;
     },
     renderForm(state) {
         this.todoFormElement.innerHTML = `<div class="input-group">
@@ -91,7 +101,7 @@ const app = {
             <h2>${todo.content}</h2>
             <div class="btn-group">
               
-                <button class="btn btn-toggle" onclick="app.toggleTodoHandel(${
+                <button class="btn btn-toggle" onclick="app.toggleTodoIsDoneHandel(${
                     todo.id
                 })">&#10004;</button>
                 <button class="btn btn-edit" onclick="app.openEditModal(${
